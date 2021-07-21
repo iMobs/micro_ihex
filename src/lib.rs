c7ee5bb306b4085fd0d9367f60c5d0ce8a41a2e3
Ian Mobley <imobley@taskrabbit.com>
2021-07-21 16:57:50 -0700

    Tests for serializing

--- src/lib.rs
+++ src/lib.rs
@@ -59,7 +59,7 @@ impl IHex {
         let expected_checksum = bytes[length - 1];
         let bytes = &bytes[..length - 1];
 
-        let checksum = 0u8.wrapping_sub(checksum(bytes));
+        let checksum = checksum(bytes);
 
         if checksum != expected_checksum {
             return Err(IHexError::BadChecksum(checksum, expected_checksum));
@@ -131,7 +131,7 @@ impl IHex {
         }
     }
 
-    pub fn serialize<T>(&self, buffer: &mut T)
+    pub fn serialize<T>(&self, buffer: &mut T) -> usize
     where
         T: AsMut<[u8]>,
     {
@@ -176,15 +176,14 @@ impl IHex {
 }
 
 fn checksum(bytes: &[u8]) -> u8 {
-    bytes.iter().fold(0u8, |acc, &byte| acc.wrapping_add(byte))
+    0u8.wrapping_sub(bytes.iter().fold(0u8, |acc, &byte| acc.wrapping_add(byte)))
 }
 
-fn format<T>(record_type: u8, offset: u16, data: &[u8], buffer: &mut T)
+fn format<T>(record_type: u8, offset: u16, data: &[u8], buffer: &mut T) -> usize
 where
     T: AsMut<[u8]>,
 {
     let buffer = buffer.as_mut();
-    let mut bytes = [0; 0x200];
     let data_length = 1 + 2 + 1 + data.len() + 1;
 
     let buffer_length = 2 * data_length + 1;
@@ -192,17 +191,21 @@ where
         // Freak out
     }
 
+    let mut bytes = [0; 0x200];
     bytes[0] = data.len() as u8;
     bytes[1..3].copy_from_slice(&offset.to_be_bytes());
     bytes[3] = record_type;
     bytes[4..data_length - 1].copy_from_slice(data);
-    bytes[data_length - 1] = checksum(&bytes[..data_length - 1]);
+    let checksum = checksum(&bytes[..data_length - 1]);
+    bytes[data_length - 1] = checksum;
 
     buffer[0] = b':';
 
     if hex::encode_to_slice(&bytes[..data_length], &mut buffer[1..buffer_length]).is_err() {
         // Freak out
     }
+
+    buffer_length
 }
 
 #[cfg(test)]
@@ -266,5 +269,79 @@ mod tests {
             IHex::parse(":0400000512345678E3"),
             Ok(IHex::StartLinearAddress(0x12345678))
         );
+    }
+
+    #[test]
+    fn serialize_data() {
+        let expected = [
+            0x61, 0x64, 0x64, 0x72, 0x65, 0x73, 0x73, 0x20, 0x67, 0x61, 0x70,
+        ];
+
+        let mut bytes = [0; 0xFF];
+        bytes[..expected.len()].copy_from_slice(&expected);
+
+        let record = IHex::Data {
+            bytes,
+            length: expected.len() as u8,
+            offset: 0x0010,
+        };
+
+        let mut buffer = [0; 0x200];
+        let length = record.serialize(&mut buffer);
+
+        assert_eq!(&buffer[..length], b":0b0010006164647265737320676170a7");
+    }
+
+    #[test]
+    fn serialize_eof() {
+        let record = IHex::EndOfFile;
+
+        let mut buffer = [0; 0x200];
+        let length = record.serialize(&mut buffer);
+
+        assert_eq!(&buffer[..length], b":00000001ff");
+    }
+
+    #[test]
+    fn serialize_extended_segment_address() {
+        let record = IHex::ExtendedSegmentAddress(0x12FE);
+
+        let mut buffer = [0; 0x200];
+        let length = record.serialize(&mut buffer);
+
+        assert_eq!(&buffer[..length], b":0200000212feec");
+    }
+
+    #[test]
+    fn serialize_start_segment_address() {
+        let record = IHex::StartSegmentAddress {
+            cs: 0x1234,
+            ip: 0x3800,
+        };
+
+        let mut buffer = [0; 0x200];
+        let length = record.serialize(&mut buffer);
+
+        assert_eq!(&buffer[..length], b":04000003123438007b");
+    }
+
+    #[test]
+    fn serialize_extended_linear_address() {
+        let record = IHex::ExtendedLinearAddress(0xABCD);
+
+        let mut buffer = [0; 0x200];
+        let length = record.serialize(&mut buffer);
+
+        assert_eq!(&buffer[..length], b":02000004abcd82");
+    }
+
+    #[test]
+    fn serialize_start_linear_address() {
+        let record = IHex::StartLinearAddress(0x12345678);
+
+        let mut buffer = [0; 0x200];
+        let length = record.serialize(&mut buffer);
+
+        assert_eq!(&buffer[..length], b":0400000512345678e3");
     }
 }
